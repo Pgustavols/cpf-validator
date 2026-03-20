0d11d7032549c121d4f8a68029f629f2cc85aeee
Paulo Gustavo <64154703+Pgustavols@users.noreply.github.com>
2026-03-19 21:14:19 -0300

    Update CpfValidator.java

--- src/main/java/CpfValidator.java
+++ src/main/java/CpfValidator.java
@@ -3,13 +3,10 @@ public class CpfValidator {
     public boolean validate(String cpf) {
         if(cpf == null || cpf.isBlank()) return false;
 
-
-
         cpf = cpf.replaceAll("[.\\\\-]", "");
 
         if(cpf.length() != 11) return false;
         if(!cpf.matches("\\d{11}")) return false;
-
 
         if(cpf.chars().distinct().count() == 1) return false;
         if(!DigitVerifier.check(cpf, 1)) return false;
